6e0e1cae390e3d48173e28450696c5cb821e0182
Cardly007 <151196162+Cardly007@users.noreply.github.com>
2023-12-08 04:19:41 +0100

    test

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-     from flask import Flask
-     app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
-     @app.route("/")
-     def hello_world():
-         return "<h1 style='color:green'>Hello World!</h1>"
+@app.route("/")
+def hello_world():
+    return "<h1 style='color:green'>Hello World!</h1>"
 
-     if __name__ == "__main__":
-         app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
